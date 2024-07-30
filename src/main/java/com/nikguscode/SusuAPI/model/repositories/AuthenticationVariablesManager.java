4959b68aec4507044538cdc869610bd2c8a9f6b4
nikguscode <nikguscode@gmail.com>
2024-07-31 02:27:42 +0500

    authentication refactor

--- src/main/java/com/nikguscode/SusuAPI/model/repositories/AuthenticationVariablesManager.java
+++ src/main/java/com/nikguscode/SusuAPI/model/repositories/AuthenticationVariablesManager.java
@@ -1,25 +1,22 @@
 package com.nikguscode.SusuAPI.model.repositories;
 
-import com.nikguscode.SusuAPI.model.repositories.SelectRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
-public class AuthenticationVariables {
-    private final SelectRepository selectRepository;
+public class AuthenticationVariablesManager {
+    private final SelectQueriesManager selectQueriesManager;
 
     @Autowired
-    public AuthenticationVariables(SelectRepository selectRepository) {
-        this.selectRepository = selectRepository;
+    public AuthenticationVariablesManager(SelectQueriesManager selectQueriesManager) {
+        this.selectQueriesManager = selectQueriesManager;
     }
 
     public Map<String, String> getVariables() {
-        Map<String, Object> query = selectRepository.executeSelectQuery();
+        Map<String, Object> query = selectQueriesManager.executeSelectQuery();
         Map<String, String> variables = new HashMap<>();
 
         for (Map.Entry<String, Object> entry : query.entrySet()) {
