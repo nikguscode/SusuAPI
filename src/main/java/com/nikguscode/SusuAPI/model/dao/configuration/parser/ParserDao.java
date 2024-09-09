72bffb8f7b915980e259e249af793f4d9f3c1745
nikguscode <nikguscode@gmail.com>
2024-09-09 11:28:59 +0500

    removed files

--- src/main/java/com/nikguscode/SusuAPI/model/dao/configuration/parser/ParserDao.java
+++
@@ -1,9 +0,0 @@
-package com.nikguscode.SusuAPI.model.dao.configuration.parser;
-
-import com.nikguscode.SusuAPI.model.entities.configuration.Parser;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ParserDao {
-    Parser get(String id);
-}
